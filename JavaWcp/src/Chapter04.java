ca816066928fdb69d28143667420276c009084f9
kohayan <disney.2381509@gmail.com>
2020-06-27 13:09:32 +0900

    Finish Chapter04

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "Daichi";
+		System.out.println(name);
+		name = "ラーメン";
+		System.out.println(name);
+
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
