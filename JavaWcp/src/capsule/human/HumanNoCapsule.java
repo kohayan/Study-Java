b45bb505f47a001427b4bcf134dbefc22dbb618c
kohayan <disney.2381509@gmail.com>
2020-06-30 21:39:01 +0900

    Study capsule

--- JavaWcp/src/capsule/human/HumanNoCapsule.java
+++ JavaWcp/src/capsule/human/HumanNoCapsule.java
@@ -1,5 +1,11 @@
 package capsule.human;
 
 public class HumanNoCapsule {
+	public String name;
+	public int age;
 
+	public HumanNoCapsule(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 }
