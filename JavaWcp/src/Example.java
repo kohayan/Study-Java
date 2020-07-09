9481314d86f42c4a1028d25e73eb171e4425b015
kohayan <disney.2381509@gmail.com>
2020-07-09 23:18:57 +0900

    Study StaticInnerClass

--- JavaWcp/src/Example.java
+++ JavaWcp/src/Example.java
@@ -1,8 +1,12 @@
 import inner_class_sample.Outer;
+import inner_class_sample.Outer2;
 
 public class Example {
 	public static void main(String[] args) {
 		Outer outer = new Outer();
 		Outer.Inner inner = outer.new Inner();
+
+		Outer2 outer2 = new Outer2();
+		Outer2.Inner2 inner2 = new Outer2.Inner2();
 	}
 }
---
+++ JavaWcp/src/inner_class_sample/Outer2.java
@@ -0,0 +1,6 @@
+package inner_class_sample;
+
+public class Outer2 {
+	public static class Inner2 {
+	}
+}
